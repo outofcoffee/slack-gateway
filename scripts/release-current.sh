50372c165de39f15e8b22a1ac413e5a18f438e37
Pete Cornish <outofcoffee@gmail.com>
2020-02-25 10:33:46 +0000

    Automatically releases tagged versions.

--- scripts/release-current.sh
+++ scripts/release-current.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="${SCRIPT_DIR}/../"
+CONFIRMATION=""
+
+while getopts "y" OPT; do
+    case ${OPT} in
+        y) CONFIRMATION="y"
+        ;;
+    esac
+done
+shift $((OPTIND-1))
 
 cd ${ROOT_DIR}
 CURRENT_VERSION="$( git describe --tags --exact-match )"
@@ -11,15 +20,17 @@ if [[ "${CURRENT_VERSION:0:1}" == "v" ]]; then
 	CURRENT_VERSION="$( echo ${CURRENT_VERSION} | cut -c 2- )"
 fi
 
-read -p "Ready to release version ${CURRENT_VERSION} (y/N)?" CONFIRMATION
+if [[ -z "${CONFIRMATION}" ]]; then
+    read -p "Ready to release version ${CURRENT_VERSION} (y/N)?" CONFIRMATION
+fi
 if [[ "y" != "${CONFIRMATION}" ]]; then
-	exit 1
+    exit 1
 fi
 
-echo "\nBuilding distribution"
+echo -e "\nBuilding distribution"
 cd ${ROOT_DIR}
 ./gradlew clean installdist
 
-echo "\nPackaging and pushing"
+echo -e "\nPackaging and pushing"
 cd ${SCRIPT_DIR}
 ./docker-build.sh ${CURRENT_VERSION}
